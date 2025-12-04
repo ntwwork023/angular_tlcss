bead991088ffc7588a94b4d9e7aedb214acf67b2
ntwwork023 <ntwwork023@gmail.com>
2025-12-04 13:37:52 +0700

    edit-profile

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DialogDataExampleDialog } from '../home/dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profile',
@@ -13,20 +15,47 @@ import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, 
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Profile { 
-  formData! : FormGroup;
+  formProfile! : FormGroup;
   isLoading : Boolean = false;
-  constructor(public formBuilder: FormBuilder) {
+  constructor(public formBuilder: FormBuilder,private dialog: MatDialog) {
     
   }
 
   ngOnInit() {
-    this.formData = this.formBuilder.group({
-      email: new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required])
-    });
-  }
+    this.formProfile = this.formBuilder.group({
+      username: new FormControl('',[Validators.required]),
+      about: new FormControl(''),
+      fname: new FormControl('', [Validators.required]),
+      lname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      region: new FormControl('', [Validators.required]),
+      pcode: new FormControl('', [Validators.required]),
+      comments: new FormControl(''),
+      candidates: new FormControl(''),
+      offers: new FormControl(''),
+      peverything: new FormControl(''),
+      pemail: new FormControl(''),
+      pnothing: new FormControl('')
+  });
+}
   onSubmit(){
-    console.log(this.formData.value);
-    
+    this.openDialog();
+    if(this.formProfile.valid){
+      console.log(this.formProfile.controls['username'].value);
+    } else{
+      
+    }   
   }
+  
+  openDialog() {
+      this.dialog.open(DialogDataExampleDialog, {
+        data:{
+          formProfile: this.formProfile.value,
+        },
+        disableClose: false,
+      });
+    }
 }
