de0b6d4a329ace97b6a1407db8a8a1db523c72a2
ntwwork023 <ntwwork023@gmail.com>
2025-12-17 12:01:26 +0700

    edit

--- src/app/pdashboard/pdashboard.ts
+++ src/app/pdashboard/pdashboard.ts
@@ -7,7 +7,7 @@ import {
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { ChartComponent } from '../chart/chart.component';
-import { Title } from 'chart.js';
+import { Tooltip } from 'chart.js';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -49,23 +49,13 @@ export class Pdashboard implements AfterViewInit {
             {
               label: '# of Votes',
               data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
+              backgroundColor: 
+                '#00DBA3',
               borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
+                '#00DBA3',
               ],
               borderWidth: 1,
+              borderRadius: 10,
             },
           ],
         },
@@ -78,6 +68,9 @@ export class Pdashboard implements AfterViewInit {
             title: {
               display: true,
               text: 'Chart.js Bar Chart',
+            },
+             tooltip: {
+                 enabled: true,
             },
           },
           scales: {
