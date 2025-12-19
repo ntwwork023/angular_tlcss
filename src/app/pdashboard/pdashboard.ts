3f04fd6f483d2cee6dc5e382c15736d1ae0bbffe
ntwwork023 <ntwwork023@gmail.com>
2025-12-19 12:07:18 +0700

    edit

--- src/app/pdashboard/pdashboard.ts
+++ src/app/pdashboard/pdashboard.ts
@@ -6,18 +6,42 @@ import { Tooltip } from 'chart.js';
 import { CommonModule } from '@angular/common';
 import { Chart } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import {MatTableModule} from '@angular/material/table';
+
 Chart.register(ChartDataLabels);
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-pdashboard',
-  imports: [MatButtonToggleModule, MatCardModule, ChartComponent, CommonModule],
+  imports: [MatButtonToggleModule, MatCardModule, ChartComponent, CommonModule, MatTableModule],
   templateUrl: './pdashboard.html',
-  styleUrl: './pdashboard.scss',
+  styleUrl: './pdashboard.scss', 
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Pdashboard implements AfterViewInit {
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
   checked = 1;
-  
   chartData: any = { labels: [], datasets: [] };
 
   dataEnvironmental = [
